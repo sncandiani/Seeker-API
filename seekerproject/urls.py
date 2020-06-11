fe738a57e66111c1c871fd055066c8fc3976cbbb
sncandiani <sofiancandiani@gmail.com>
2020-06-11 11:54:32 -0500

    Created Register functionality and tested through postman

--- seekerproject/urls.py
+++ seekerproject/urls.py
@@ -14,8 +14,18 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+# from rest_framework import routers
+from rest_framework.authtoken.views import obtain_auth_token
+from seekerapp.models import *
+from seekerapp.views import *
+
+# router = routers.DefaultRouter(trailing_slash=False)
 
 urlpatterns = [
+    # path('', include(router.urls)),
     path('admin/', admin.site.urls),
+    path('api-token-auth/', obtain_auth_token),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    path('register', register_user)
 ]
